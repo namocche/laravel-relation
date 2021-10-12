8b75c98075bdf3bd9d8ec1aa7ce0756191b4d54e
なもちゃん <user@usernoMacBook-Air.local>
2021-10-12 14:52:01 +0900

    first-commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,35 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\ClientRequest;
 
 class TestController extends Controller
-//  {
-//       public function index() 
-// {
-//         $item = [
-//             'content' => '自由に入力してください',
-//         ];
-//         return view('index', $item);
-//     }
-//     public function post(Request $request)
-//     {
-//         $content = $request->content;
-//         $item = [
-//             'content' => $content . 'と入力しましたね'
-//         ];
-//         return view('index', $item);
-//     }
-// }
-
 {
     public function index(Request $request)
     {
         return view('index', ['txt' => 'フォームを入力']);
     }
-    public function post(ClientRequest $request)
+    public function post(Request $request)
     {
+        $validate_rule = [
+            'name' => 'required',
+            'role' => 'required',
+            'email' => 'required|email',
+            'age' => 'numeric',
+            'registered_at' => 'date|nullable',
+        ];
+        $this->validate($request, $validate_rule);
         return view('index', ['txt' => '正しい入力です']);
     }
 }
-
